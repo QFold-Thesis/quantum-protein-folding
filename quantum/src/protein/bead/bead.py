2084047089efc49a289ff457ffb487b0bfc1ca14
unknown <stefoo123@outlook.com>
2025-09-18 20:14:08 +0200

    Refactored code, applied ruff rules, added logging

--- quantum/src/protein/bead/bead.py
+++ quantum/src/protein/bead/bead.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-
-from qiskit.quantum_info import (  # pyright: ignore[reportMissingTypeStubs]
-    SparsePauliOp,
-)
+from typing import TYPE_CHECKING
 
 from constants import CONFORMATION_ENCODING, QUBITS_PER_TURN
 from enums import ConformationEncoding, SubLattice
 from exceptions import ConformationEncodingError
 from logger import get_logger
 from utils.qubit_utils import build_full_identity, build_turn_qubit
+
+if TYPE_CHECKING:
+    from qiskit.quantum_info import (
+        SparsePauliOp,
+    )
 
 logger = get_logger()
 
@@ -81,7 +83,9 @@ class Bead(ABC):
             return
         raise ConformationEncodingError
 
-    def turn_funcs(self) -> None | tuple[SparsePauliOp, SparsePauliOp, SparsePauliOp, SparsePauliOp]:
+    def turn_funcs(
+        self,
+    ) -> None | tuple[SparsePauliOp, SparsePauliOp, SparsePauliOp, SparsePauliOp]:
         if self.turn_qubits is None:
             return None
         return (self.turn_0(), self.turn_1(), self.turn_2(), self.turn_3())
