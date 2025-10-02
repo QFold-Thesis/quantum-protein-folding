ba0ab1bd6c825c528f1c76035da4bcfd63f97404
Lucjan Gackowski <lucjan.gackowski@gmail.com>
2025-10-02 20:15:30 +0200

    main.py refactored

--- quantum/src/main.py
+++ quantum/src/main.py
@@ -7,48 +7,53 @@ from builder import HamiltonianBuilder
 from constants import EMPTY_SIDECHAIN_PLACEHOLDER
 from contact import ContactMap
 from distance import DistanceMap
-from interaction import HPInteraction, MJInteraction  # noqa: F401
+from interaction import HPInteraction, Interaction, MJInteraction
+from logger import get_logger
 from protein import Protein
 from utils.qubit_utils import remove_unused_qubits
 
+logger = get_logger()
 
-def main() -> None:
-    main_chain = "APRLRFY"
-    side_chain = EMPTY_SIDECHAIN_PLACEHOLDER * len(main_chain)
 
+def setup_folding_system(main_chain: str, side_chain: str) -> tuple[Protein, MJInteraction, ContactMap, DistanceMap]:
+    """Setup the protein folding system components."""
     protein = Protein(
         main_protein_sequence=main_chain, side_protein_sequence=side_chain
     )
-
     mj_interaction = MJInteraction()
-    # hp_interaction = HPInteraction()  # noqa: ERA001
+    hp_interaction = HPInteraction()  # noqa: F841
 
     contact_map = ContactMap(protein=protein)
-
     distance_map = DistanceMap(protein=protein)
 
+    return protein, mj_interaction, contact_map, distance_map
+
+
+def build_and_compress_hamiltonian(
+    protein: Protein,
+    interaction: Interaction,
+    contact_map: ContactMap,
+    distance_map: DistanceMap
+) -> tuple[object, object]:
+    """Build and compress the Hamiltonian."""
     h_builder = HamiltonianBuilder(
         protein=protein,
-        interaction=mj_interaction,
+        interaction=interaction,
         distance_map=distance_map,
         contact_map=contact_map,
     )
 
     hamiltonian = h_builder.sum_hamiltonians()
-    print(  # noqa: T201
-        40 * "-",
-        "\n Original hamiltonian qubits:",
-        hamiltonian.num_qubits,
-    )
+    logger.debug("Original hamiltonian qubits: %d", hamiltonian.num_qubits)
 
     compressed_h = remove_unused_qubits(hamiltonian)
-    print(  # noqa: T201
-        "\n Compressed hamiltonian qubits:",
-        compressed_h.num_qubits,
-        "\n",
-        40 * "-",
-    )
+    logger.debug("Compressed hamiltonian qubits: %d", compressed_h.num_qubits)
 
+    return hamiltonian, compressed_h
+
+
+def setup_vqe_optimization(compressed_h: object) -> tuple[object, list, list]:
+    """Setup VQE optimization components."""
     optimizer = COBYLA(maxiter=50)
     ansatz = real_amplitudes(num_qubits=compressed_h.num_qubits, reps=1)
 
@@ -67,8 +72,28 @@ def main() -> None:
         callback=_store_intermediate_result,
     )
 
+    return vqe, counts, values
+
+
+def main() -> None:
+    main_chain: str = "APRLRFY"
+    side_chain: str = EMPTY_SIDECHAIN_PLACEHOLDER * len(main_chain)
+
+    protein, interaction, contact_map, distance_map = setup_folding_system(main_chain, side_chain)
+
+    hamiltonian, compressed_h = build_and_compress_hamiltonian(
+        protein=protein,
+        interaction=interaction,
+        contact_map=contact_map,
+        distance_map=distance_map
+    )
+
+    vqe, counts, values = setup_vqe_optimization(compressed_h)
+
+    logger.debug("Starting VQE optimization...")
     raw_result = vqe.compute_minimum_eigenvalue(compressed_h)
-    print(raw_result)  # noqa: T201
+
+    logger.debug(raw_result)
 
 
 if __name__ == "__main__":
